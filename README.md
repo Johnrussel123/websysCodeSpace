7a86a3f93d46d4f464720da8fffba505c99a5d59
Krist Raymund <johnrusselgwapings@gmail.com>
2025-08-13 02:13:43 +0000

    main php syntax

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# websysCodeSpace
+
---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$name  = "John Russel Leyson";
+$age = "21 ";
+$school = "Global Reciprocal Colleges";
+$course = "Information System";  
+$subject = "Websys";
+
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "School: " . $school . "<br>";
+echo "Course: " . $course . "<br>";
+echo "Subject: " . $subject . "<br>";
+?>
+
+</body>
+</html>
